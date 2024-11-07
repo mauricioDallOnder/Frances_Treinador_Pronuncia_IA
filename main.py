3ab713d0b46dcb78c76a867a047293614c566407
Mauricio Dall Onder <londer11@icloud.com>
2024-11-06 22:42:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -486,4 +486,4 @@ def get_progress():
 
 # Inicialização e execução do aplicativo
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(host='0.0.0.0', port=os.getenv("PORT", default=5000))
